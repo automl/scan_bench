7bea5d6465a738eb48ea343f0930b88cb0a7b0dc
Donat <donatsinani70@gmail.com>
2026-06-12 18:50:01 +0200

    update tabpfn space

--- scan_benchmark/tabpfn/spaces.py
+++ scan_benchmark/tabpfn/spaces.py
@@ -16,7 +16,7 @@ TABPFN_SEARCH_SPACE = {
         "total_cells": {
             "type": "int",
             "lower": 1048576,
-            "upper": 1073741824,
+            "upper": 34359738368,
         },
         "embedding_size": {
             "choices": [4, 8, 16, 32, 64, 128, 256],
